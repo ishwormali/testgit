a68ce6f3e8ebcc07ab30447f8e1d8ab8612b7410
Ishwor Mali <ishwor.mali@terem.com.au>
2018-06-09 18:44:04 +1000

    all eature 3 features

---
+++ src/app/feature3.ts
@@ -0,0 +1,14 @@
+export default class Feature3 {
+  constructor(){
+    console.log('constructor');
+  }
+
+  feature3() {
+    console.log('feature 3 new function');
+  }
+
+  feature4() {
+    console.log('feature3 new fn');
+
+  }
+}
